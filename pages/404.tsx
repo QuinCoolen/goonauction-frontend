79730805fd28666f4d4eb10977b1f58d5efced01
Quin Coolen <quin.coolen@gmail.com>
2025-09-18 15:39:46 +0200

    Change a tags to next Link tags

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Custom404() {
   return (
     <div className="flex flex-col items-center justify-center h-[50vh] bg-gray-100">
       <h1 className="text-6xl font-bold text-gray-800">404</h1>
       <p className="text-2xl text-gray-600 mt-4">Page Not Found</p>
-      <a href="/" className="mt-6 text-primary hover:underline">
+      <Link href="/" className="mt-6 text-primary hover:underline">
         Go back to Home
-      </a>
+      </Link>
     </div>
   );
 }
